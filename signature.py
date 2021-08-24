0a2bce4293786a1b006fceb0fda91a350ac6e206
JL Bautista <joseluis.bautista@ujed.mx>
2021-08-24 15:32:34 -0500

    Commit de inicialización

---
+++ signature.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env vpython3
+# *-* coding: utf-8 *-*
+import sys
+import datetime
+from cryptography.hazmat import backends
+from cryptography.hazmat.primitives.serialization import pkcs12
+
+from endesive.pdf import cms
+
+# from endesive.pdf import cmsn as cms
+
+# import logging
+# logging.basicConfig(level=logging.DEBUG)
+
+
+def main():
+    date = datetime.datetime.utcnow() - datetime.timedelta(hours=12)
+    date = date.strftime("D:%Y%m%d%H%M%S+00'00'")
+    dct = {
+        "aligned": 0,
+        "sigflags": 3,
+        "sigflagsft": 132,
+        "sigpage": 0,
+        "sigbutton": True,
+        "sigfield": "Signature1",
+        "auto_sigfield": True,
+        "sigandcertify": True,
+        "signaturebox": (0, 840, 570, 640),
+        "signature": "José Luis Bautista Cabrera",
+#        "signature_img": "firma_JL.png",
+        "contact": "jl.bautistac@gmail.com",
+        "location": "Durango, MX",
+        "signingdate": date,
+        "reason": "Firma de documento",
+        "password": "1234",
+    }
+    with open("cert.p12", "rb") as fp:
+        p12 = pkcs12.load_key_and_certificates(
+            fp.read(), b"1234", backends.default_backend()
+        )
+    fname = "oficio_firma.pdf"
+    if len(sys.argv) > 1:
+        fname = sys.argv[1]
+    datau = open(fname, "rb").read()
+    datas = cms.sign(datau, dct, p12[0], p12[1], p12[2], "sha256")
+    fname = fname.replace(".pdf", "-firmado.pdf")
+    with open(fname, "wb") as fp:
+        fp.write(datau)
+        fp.write(datas)
+
+
+main()
